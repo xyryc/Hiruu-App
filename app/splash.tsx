ec04ddd170d808c69f0dc2fce84fa3f3df84b51c
xyryc <amdanikzz@gmail.com>
2025-09-02 06:42:57 +0600

    update

--- app/splash.tsx
+++ app/splash.tsx
@@ -1,7 +1,6 @@
 import { SplashScreenProps } from "@/types";
-import { LinearGradient } from "expo-linear-gradient";
 import React, { useEffect, useState } from "react";
-import { Animated, Image, Text, View } from "react-native";
+import { Animated, Image } from "react-native";
 
 const SplashScreen = ({ onFinish }: SplashScreenProps) => {
   const [fadeAnim] = useState(new Animated.Value(1));
@@ -13,37 +12,19 @@ const SplashScreen = ({ onFinish }: SplashScreenProps) => {
         duration: 500,
         useNativeDriver: true,
       }).start(() => onFinish());
-    }, 500);
+    }, 1500);
   }, []);
 
   return (
-    <Animated.View
-      className="flex-1 justify-center items-center bg-gradient-to-b from-blue-100 to-gray-100"
-      style={{ opacity: fadeAnim }}
-    >
-      <LinearGradient
-        colors={["#E3F2FD", "#F5F5F5"]}
-        className="flex-1 justify-center items-center w-full"
-      >
-        {/* Logo */}
-        <View className="items-center mb-8">
-          <Image
-            style={{
-              width: 124,
-              height: 44,
-            }}
-            source={require("@/assets/images/hiruu-logo.svg")}
-            resizeMode="contain"
-          />
-        </View>
-
-        {/* Tagline at bottom */}
-        <View className="absolute bottom-12 items-center px-4">
-          <Text className="text-gray-600 text-sm font-light text-center">
-            Your Daily Work Companion...
-          </Text>
-        </View>
-      </LinearGradient>
+    <Animated.View style={{ opacity: fadeAnim }}>
+      <Image
+        style={{
+          width: 1000,
+          height: 720,
+        }}
+        source={require("@/assets/images/splash.svg")}
+        resizeMode="contain"
+      />
     </Animated.View>
   );
 };
