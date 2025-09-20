a5c5730b13af5e8ae8de79a764812d8c63f8abb9
xyryc <amdanikzz@gmail.com>
2025-09-21 02:06:19 +0600

    modified stats card ui

--- components/ui/cards/StatCardSecondary.tsx
+++ components/ui/cards/StatCardSecondary.tsx
@@ -8,7 +8,7 @@ import SimpleStatusBadge from "../badges/SimpleStatusBadge";
 const StatCardSecondary = ({ background }: StatCardSecondaryProps) => {
   return (
     <View className="flex-row justify-between border border-[#EEEEEE] rounded-xl overflow-hidden">
-      <View className="p-4">
+      <View className="px-4 py-5">
         <Text className="text-sm font-proximanova-regular text-secondary">
           Completed
         </Text>
@@ -29,14 +29,14 @@ const StatCardSecondary = ({ background }: StatCardSecondaryProps) => {
 
       <View className="justify-end">
         <SimpleStatusBadge
-          className="absolute top-3 right-24"
+          className="absolute top-4 right-24 -rotate-12"
           title="On Time"
           textColor="#6998EF"
           bgColor="#E9F0FD"
         />
 
         <SimpleStatusBadge
-          className="absolute top-1.5 right-2"
+          className="absolute top-2 right-2 rotate-6"
           title="Reliable"
           textColor="#3EBF5A"
           bgColor="#3EBF5A1F"
@@ -56,7 +56,7 @@ const StatCardSecondary = ({ background }: StatCardSecondaryProps) => {
           source={background}
           style={{
             width: 100,
-            height: 100,
+            height: 120,
           }}
           contentFit="scale-down"
         />
