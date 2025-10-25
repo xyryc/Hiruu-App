83e4faed40ba8a9521f73d0e687d82ed8396620c
mdalifkhandev <mdalifk2002@gmail.com>
2025-10-26 01:25:47 +0600

    standard challenges screen

--- app/(user)/rewards/challenges.tsx
+++ app/(user)/rewards/challenges.tsx
@@ -1,5 +1,5 @@
 import ScreenHeader from '@/components/header/ScreenHeader'
-import { MaterialIcons } from '@expo/vector-icons'
+import { FontAwesome6, MaterialIcons } from '@expo/vector-icons'
 import { Image } from 'expo-image'
 import { router } from 'expo-router'
 import { useColorScheme } from 'nativewind'
@@ -52,6 +52,260 @@ const challenges = () => {
                             </TouchableOpacity>
                         ))
                     }
+                </View>
+                {/* challenges list */}
+                <View className='mx-5 flex-row gap-4 mt-4'>
+                    <View>
+                        <Image source={require('@/assets/images/reward/complate-profile.svg')} contentFit='contain' style={{ height: 87, width: 63 }} />
+                    </View>
+                    <View>
+                        <View className='flex-row justify-between items-center'>
+                            <Text className='font-proximanova-semibold text-primary dark:text-dark-primary mt-2'>Complete 100% profile</Text>
+                            <View className=' bg-[#11293A] top-4 rounded-full'>
+                                <Text className='px-4 py-2 font-proximanova-semibold text-sm text-[#ffffff] text-center'>Complete</Text>
+                            </View>
+                        </View>
+                        <View className="flex-row items-center ">
+                            <Image
+                                source={require("@/assets/images/hiruu-coin.svg")}
+                                style={{
+                                    width: 22,
+                                    height: 22,
+                                }}
+                                contentFit="contain"
+                            />
+                            <View className="px-4 py-1 bg-[#DDF1FF] -ml-3 -z-10 rounded-r-[40px]">
+                                <Text className="text-xs font-proximanova-semibold">10</Text>
+                            </View>
+                            <FontAwesome6 name="crown" className='ml-2' size={15} color="#4FB2F3" />
+                        </View>
+                        <View className="flex-row items-center gap-3 justify-between mt-2">
+                            <View className="w-[220px] bg-[#DDF1FF] -z-10 rounded-[40px]">
+                                <Image
+                                    source={require("@/assets/images/hiruu-coin.svg")}
+                                    style={{
+                                        width: 22,
+                                        height: 22,
+                                    }}
+                                    contentFit="contain"
+                                />
+
+                            </View>
+                            <Text className='font-proximanova-regular text-sm text-primary dark:text-dark-primary'><Text className='text-[#4FB2F3]'>0</Text>/1</Text>
+
+                        </View>
+                    </View>
+                </View>
+                <Image source={require('@/assets/images/dotted-line.svg')} contentFit='contain' style={{ width: 320, height: 2, marginHorizontal: 'auto', marginTop: 20 }} />
+                {/* 2 */}
+                <View className='mx-5 flex-row gap-4 mt-5'>
+                    <View>
+                        <Image source={require('@/assets/images/reward/seven-day.svg')} contentFit='contain' style={{ height: 87, width: 63 }} />
+                    </View>
+                    <View>
+                        <View className='flex-row justify-between items-center'>
+                            <Text className='font-proximanova-semibold text-primary dark:text-dark-primary mt-2'>Log In Daily for 7 Days</Text>
+                            <View className=' bg-[#11293A] top-4 rounded-full'>
+                                <Text className='px-4 py-2 font-proximanova-semibold text-sm text-[#ffffff] text-center'>Upload</Text>
+                            </View>
+                        </View>
+                        <View className="flex-row items-center ">
+                            <Image
+                                source={require("@/assets/images/hiruu-coin.svg")}
+                                style={{
+                                    width: 22,
+                                    height: 22,
+                                }}
+                                contentFit="contain"
+                            />
+                            <View className="px-4 py-1 bg-[#DDF1FF] -ml-3 -z-10 rounded-r-[40px]">
+                                <Text className="text-xs font-proximanova-semibold">20</Text>
+                            </View>
+                            <FontAwesome6 name="crown" className='ml-2' size={15} color="#4FB2F3" />
+                        </View>
+                        <View className="flex-row items-center gap-3 justify-between mt-2">
+                            <View className="w-[220px] bg-[#DFDFDF] -z-10 rounded-[40px]">
+                                <Image
+                                    source={require("@/assets/images/hiruu-coin.svg")}
+                                    style={{
+                                        width: 22,
+                                        height: 22,
+                                        left: 200
+                                    }}
+                                    contentFit="contain"
+                                />
+
+                            </View>
+                            <Text className='font-proximanova-regular text-sm text-primary dark:text-dark-primary'><Text className='text-[#4FB2F3]'>7</Text>/7</Text>
+
+                        </View>
+                    </View>
+                </View>
+                <Image source={require('@/assets/images/dotted-line.svg')} contentFit='contain' style={{ width: 320, height: 2, marginHorizontal: 'auto', marginTop: 20 }} />
+                {/* 3 */}
+                <View className='mx-5 flex-row gap-4 mt-5'>
+                    <View>
+                        <Image source={require('@/assets/images/reward/upload.svg')} contentFit='contain' style={{ height: 87, width: 63 }} />
+                    </View>
+                    <View>
+                        <View className='flex-row justify-between items-center'>
+                            <Text className='font-proximanova-semibold text-primary dark:text-dark-primary mt-2'>Upload Profile Picture</Text>
+                            <View className=' bg-[#11293A] top-4 rounded-full'>
+                                <Text className='px-4 py-2 font-proximanova-semibold text-sm text-[#ffffff] text-center'>Buy Now</Text>
+                            </View>
+                        </View>
+                        <View className="flex-row items-center ">
+                            <Image
+                                source={require("@/assets/images/hiruu-coin.svg")}
+                                style={{
+                                    width: 22,
+                                    height: 22,
+                                }}
+                                contentFit="contain"
+                            />
+                            <View className="px-4 py-1 bg-[#DDF1FF] -ml-3 -z-10 rounded-r-[40px]">
+                                <Text className="text-xs font-proximanova-semibold">20</Text>
+                            </View>
+                            <FontAwesome6 name="crown" className='ml-2' size={15} color="#4FB2F3" />
+                        </View>
+
+                        {/*  rank bord line --  */}
+
+                        <View className='flex-row justify-center items-center gap-4 mt-2.5'>
+                            <View className="relative">
+                                <View className="w-[220px] bg-[#DDF1FF] rounded-[40px] relative ">
+                                    <Image
+                                        source={require('@/assets/images/hiruu-coin.svg')}
+                                        style={{
+                                            width: 22,
+                                            height: 22,
+                                            left: 100,
+                                            zIndex: 1
+                                        }}
+                                        contentFit="contain"
+                                    />
+                                </View>
+                                <View className="bg-[#4FB2F3] py-3 w-[120px] bottom-8 rounded-full  mt-2" />
+                            </View>
+
+
+                            <Text className='font-proximanova-regular text-sm -mt-9 text-primary dark:text-dark-primary'>
+                                <Text className='text-[#4FB2F3]'>3</Text>/6
+                            </Text>
+
+
+                        </View>
+
+
+                        {/* rank bord line --  */}
+
+
+
+
+                    </View>
+                </View>
+                <Image source={require('@/assets/images/dotted-line.svg')} contentFit='contain' style={{ width: 320, height: 2, marginHorizontal: 'auto' }} />
+                {/* 4 */}
+                <View className='mx-5 flex-row gap-4 mt-5'>
+                    <View>
+                        <Image source={require('@/assets/images/reward/accept.svg')} contentFit='contain' style={{ height: 87, width: 63 }} />
+                    </View>
+                    <View>
+                        <View className='flex-row justify-between items-center'>
+                            <Text className='font-proximanova-semibold text-primary dark:text-dark-primary mt-2'>Accept First Shift</Text>
+                            <View className=' bg-[#11293A] top-4 rounded-full'>
+                                <Text className='px-4 py-2 font-proximanova-semibold text-sm text-[#ffffff] text-center'>Accept</Text>
+                            </View>
+                        </View>
+                        <View className="flex-row items-center ">
+                            <Image
+                                source={require("@/assets/images/hiruu-coin.svg")}
+                                style={{
+                                    width: 22,
+                                    height: 22,
+                                }}
+                                contentFit="contain"
+                            />
+                            <View className="px-4 py-1 bg-[#DDF1FF] -ml-3 -z-10 rounded-r-[40px]">
+                                <Text className="text-xs font-proximanova-semibold">20</Text>
+                            </View>
+                            <FontAwesome6 name="crown" className='ml-2' size={15} color="#4FB2F3" />
+                        </View>
+                        {/*  rank bord line --  */}
+
+                        <View className='flex-row justify-center items-center gap-4 mt-2.5'>
+                            <View className="relative">
+                                <View className="w-[220px] bg-[#DDF1FF] rounded-[40px] relative ">
+                                    <Image
+                                        source={require('@/assets/images/hiruu-coin.svg')}
+                                        style={{
+                                            width: 22,
+                                            height: 22,
+                                            left: 140,
+                                            zIndex: 1
+                                        }}
+                                        contentFit="contain"
+                                    />
+                                </View>
+                                <View className="bg-[#4FB2F3] py-3 w-[160px] bottom-8 rounded-full  mt-2" />
+                            </View>
+
+
+                            <Text className='font-proximanova-regular text-sm -mt-9 text-primary dark:text-dark-primary'>
+                                <Text className='text-[#4FB2F3]'>4</Text>/6
+                            </Text>
+
+
+                        </View>
+
+
+                        {/* rank bord line --  */}
+                    </View>
+                </View>
+                <Image source={require('@/assets/images/dotted-line.svg')} contentFit='contain' style={{ width: 320, height: 2, marginHorizontal: 'auto' }} />
+                {/* 5 */}
+                <View className='mx-5 flex-row gap-4 mt-5'>
+                    <View>
+                        <Image source={require('@/assets/images/reward/reted.svg')} contentFit='contain' style={{ height: 87, width: 63 }} />
+                    </View>
+                    <View>
+                        <View className='flex-row justify-between items-center'>
+                            <Text className='font-proximanova-semibold text-primary dark:text-dark-primary mt-2'>Get Rated 3 Times</Text>
+                            <View className=' bg-[#11293A] top-4 rounded-full'>
+                                <Text className='px-4 py-2 font-proximanova-semibold text-sm text-[#ffffff] text-center'>Collected</Text>
+                            </View>
+                        </View>
+                        <View className="flex-row items-center ">
+                            <Image
+                                source={require("@/assets/images/hiruu-coin.svg")}
+                                style={{
+                                    width: 22,
+                                    height: 22,
+                                }}
+                                contentFit="contain"
+                            />
+                            <View className="px-4 py-1 bg-[#DDF1FF] -ml-3 -z-10 rounded-r-[40px]">
+                                <Text className="text-xs font-proximanova-semibold">20</Text>
+                            </View>
+                            <FontAwesome6 name="crown" className='ml-2' size={15} color="#4FB2F3" />
+                        </View>
+                        <View className="flex-row items-center gap-3 justify-between mt-2">
+                            <View className="w-[220px] bg-[#DFDFDF] -z-10 rounded-[40px]">
+                                <Image
+                                    source={require("@/assets/images/hiruu-coin.svg")}
+                                    style={{
+                                        width: 22,
+                                        height: 22,
+                                        left: 200
+                                    }}
+                                    contentFit="contain"
+                                />
+
+                            </View>
+                            <Text className='font-proximanova-regular text-sm text-primary dark:text-dark-primary'><Text className='text-[#4FB2F3]'>1</Text>/1</Text>
+
+                        </View>
+                    </View>
                 </View>
             </ScrollView>
         </SafeAreaView>
