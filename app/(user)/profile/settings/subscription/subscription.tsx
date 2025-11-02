241cfac382ca668668fdc739d15f15b0173c7830
mdalifkhandev <mdalifk2002@gmail.com>
2025-11-03 04:35:53 +0600

    Subscription plane   screen main page screen complite

--- app/(user)/profile/settings/subscription/subscription.tsx
+++ app/(user)/profile/settings/subscription/subscription.tsx
@@ -1,10 +1,15 @@
 import ScreenHeader from "@/components/header/ScreenHeader";
 import SettingsCard from "@/components/ui/cards/SettingsCard";
-import { Entypo, Feather, SimpleLineIcons } from "@expo/vector-icons";
+import {
+  Entypo,
+  Feather,
+  MaterialCommunityIcons,
+  SimpleLineIcons,
+} from "@expo/vector-icons";
 import { router } from "expo-router";
 import { useColorScheme } from "nativewind";
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const Subscription = () => {
@@ -25,36 +30,133 @@ const Subscription = () => {
         />
       </View>
 
-      <View className="m-5">
-        <View className="border px-3 rounded-2xl border-[#EEEEEE]">
-          <SettingsCard
-            icon={
-              <Feather
-                name="user"
-                className="border rounded-md"
-                size={24}
-                color="black"
-              />
-            }
-            text="Subscription"
-            subtitle="Mobile"
-            className="mt-5"
-            arrowIcon={
-              <Entypo name="chevron-thin-right" size={20} color="#111111" />
-            }
-          />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View className="m-5">
+          <View className="border border-[#EEEEEE] rounded-2xl">
+            <View className="px-2.5 py-5 flex-row gap-3">
+              <View className="h-[50px] w-[50px] bg-[#11293A1A] rounded-xl flex-row justify-center items-center">
+                <MaterialCommunityIcons
+                  name="receipt-text-outline"
+                  size={22}
+                  color="black"
+                />
+              </View>
+              <View>
+                <Text className="font-proximanova-bold text-xl text-primary dark:text-dark-primary">
+                  User plan & Billing
+                </Text>
+                <Text className="font-proximanova-semibold text-sm text-secondary dark:text-dark-secondary mt-3">
+                  SarahCatlynne@gmail.com
+                </Text>
+                <Text className="font-proximanova-semibold text-sm text-primary dark:text-dark-pritext-primary mt-3">
+                  Password: *********
+                </Text>
+                <Text className="font-proximanova-semibold text-sm text-primary dark:text-dark-pritext-primary mt-3">
+                  Phone: 07896541232
+                </Text>
+                <Text className="font-proximanova-semibold text-sm text-primary dark:text-dark-pritext-primary mt-3">
+                  Active: July 25, 2025 till August 1, 2025
+                </Text>
+              </View>
+            </View>
+            <View className="border-b my-5 border-[#EEEEEE]" />
+            <View className="px-2.5 py-5">
+              <Text className="font-proximanova-bold text-xl text-primary dark:text-dark-primary text-center">
+                Billed through Googlepay
+              </Text>
+              <Text className="font-proximanova-semibold text-sm text-secondary dark:text-dark-secondary mt-3 px-14">
+                Contact Googlepay to update our payment details or end your
+                billing throut googlepay
+              </Text>
+            </View>
+            <View className="border-b my-5 border-[#EEEEEE]" />
+            <View>
+              <Text className="font-proximanova-bold text-[#F34F4F] text-center mb-5">
+                Cancel Plan
+              </Text>
+            </View>
+          </View>
+          <View className="border border-[#EEEEEE] rounded-2xl">
+            <View className="px-2.5 py-5 flex-row gap-3">
+              <View className="h-[50px] w-[50px] bg-[#11293A1A] rounded-xl flex-row justify-center items-center">
+                <MaterialCommunityIcons
+                  name="receipt-text-outline"
+                  size={22}
+                  color="black"
+                />
+              </View>
+              <View>
+                <Text className="font-proximanova-bold text-xl text-primary dark:text-dark-primary">
+                  Business plan & Billing
+                </Text>
+                <Text className="font-proximanova-semibold text-sm text-secondary dark:text-dark-secondary mt-3">
+                  SarahCatlynne@gmail.com
+                </Text>
+                <Text className="font-proximanova-semibold text-sm text-primary dark:text-dark-pritext-primary mt-3">
+                  Password: *********
+                </Text>
+                <Text className="font-proximanova-semibold text-sm text-primary dark:text-dark-pritext-primary mt-3">
+                  Phone: 07896541232
+                </Text>
+                <Text className="font-proximanova-semibold text-sm text-primary dark:text-dark-pritext-primary mt-3">
+                  Active: July 25, 2025 till August 1, 2025
+                </Text>
+              </View>
+            </View>
+            <View className="border-b my-5 border-[#EEEEEE]" />
+            <View className="px-2.5 py-5">
+              <Text className="font-proximanova-bold text-xl text-primary dark:text-dark-primary text-center">
+                Billed through Googlepay
+              </Text>
+              <Text className="font-proximanova-semibold text-sm text-secondary dark:text-dark-secondary mt-3 px-14">
+                Contact Googlepay to update our payment details or end your
+                billing throut googlepay
+              </Text>
+            </View>
+            <View className="border-b my-5 border-[#EEEEEE]" />
+            <View>
+              <Text className="font-proximanova-bold text-[#F34F4F] text-center mb-5">
+                Cancel Plan
+              </Text>
+            </View>
+          </View>
+
+          <View className="border px-3 rounded-2xl border-[#EEEEEE] mt-5">
+            <SettingsCard
+              icon={
+                <Feather
+                  name="user"
+                  className="border rounded-md"
+                  size={24}
+                  color="black"
+                />
+              }
+              text="Subscription"
+              subtitle="Mobile"
+              className="mt-5"
+              arrowIcon={
+                <Entypo name="chevron-thin-right" size={20} color="#111111" />
+              }
+            />
+          </View>
+          <View className="border px-3 rounded-2xl border-[#EEEEEE] mt-2.5">
+            <SettingsCard
+              icon={
+                <SimpleLineIcons name="briefcase" size={24} color="black" />
+              }
+              text="Subscription"
+              className="mt-5"
+              arrowIcon={
+                <Entypo name="chevron-thin-right" size={20} color="#111111" />
+              }
+            />
+          </View>
         </View>
-        <View className="border px-3 rounded-2xl border-[#EEEEEE] mt-2.5">
-          <SettingsCard
-            icon={<SimpleLineIcons name="briefcase" size={24} color="black" />}
-            text="Subscription"
-            className="mt-5"
-            arrowIcon={
-              <Entypo name="chevron-thin-right" size={20} color="#111111" />
-            }
-          />
-        </View>
-      </View>
+
+        <Text className="text-center text-secondary dark:text-dark-secondary text-sm">
+          subscription auto-renews until manually cancelled.
+        </Text>
+      </ScrollView>
     </SafeAreaView>
   );
 };
