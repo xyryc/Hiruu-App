0c97e6066d27745f9d8bb324469ed2ca92659270
mdalifkhandev <mdalifk2002@gmail.com>
2025-11-08 02:10:30 +0600

    business home is start

---
+++ components/layout/BusinessSummary.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const BusinessSummary = () => {
+  return (
+    <View>
+      <Text className="text-xl font-proximanova-semibold">Work Insights</Text>
+    </View>
+  );
+};
+
+export default BusinessSummary;
