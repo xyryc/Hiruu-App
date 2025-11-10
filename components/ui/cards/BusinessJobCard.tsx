4a885efc1f8a4c182b6568e40482a13efd43d808
xyryc <amdanikzz@gmail.com>
2025-11-11 05:59:08 +0600

    working on business job card component

---
+++ components/ui/cards/BusinessJobCard.tsx
@@ -0,0 +1,152 @@
+import { BusinessJobCardProps } from "@/types";
+import {
+  FontAwesome,
+  MaterialCommunityIcons,
+  MaterialIcons,
+  SimpleLineIcons,
+} from "@expo/vector-icons";
+import { Image } from "expo-image";
+import React from "react";
+import { Text, View } from "react-native";
+import SmallButton from "../buttons/SmallButton";
+
+const BusinessJobCard = ({ className, status }: BusinessJobCardProps) => {
+  return (
+    <View
+      className={`${className}
+      ${status === "featured" && "bg-[#E5F4FD]"}
+      p-2.5 rounded-xl border border-[#4FB2F330]`}
+    >
+      {/* top */}
+      <View className="relative">
+        {/* content */}
+        <View className="flex-row items-center gap-2.5 p-1 z-10">
+          {/* profile image */}
+          <Image
+            source="https://images.squarespace-cdn.com/content/v1/5521b031e4b06ebe90178744/1560360135937-3YXVZ3124L1YL2FOASSQ/headshots-linkedin-photographer.jpg"
+            style={{
+              width: 40,
+              height: 40,
+              borderRadius: 999,
+            }}
+            contentFit="cover"
+          />
+
+          {/* name */}
+          <View>
+            <Text className="text-base font-proximanova-semibold">
+              Cashier{" "}
+              <MaterialCommunityIcons name="crown" size={14} color="#4FB2F3" />
+            </Text>
+
+            <Text className="text-sm font-proximanova-regular">
+              Mohammad Anik
+            </Text>
+          </View>
+        </View>
+
+        {/* background */}
+        {status === "featured" && (
+          <View className="absolute top-0 left-0 w-full">
+            <Image
+              source={require("@/assets/images/featured.svg")}
+              style={{
+                width: "100%",
+                height: 50,
+                borderRadius: 10,
+              }}
+              contentFit="cover"
+            />
+          </View>
+        )}
+      </View>
+
+      {/* mid */}
+      <View className="flex-row items-center justify-between gap-20 mt-2.5">
+        <View className="flex-row items-center gap-1.5">
+          <SimpleLineIcons name="location-pin" size={12} color="black" />
+          <Text className="text-sm font-proximanova-regular text-secondary dark:text-dark-secondary">
+            New York, North Bergen
+          </Text>
+        </View>
+
+        <View className="flex-row">
+          <Text className="text-xl font-proximanova-semibold text-primary">
+            5-10$
+          </Text>
+          <Text className="text-lg font-proximanova-regular text-secondary">
+            /hr
+          </Text>
+        </View>
+      </View>
+
+      {/* badges */}
+      <View className="flex-row gap-1.5 mt-2.5">
+        <View className="flex-row gap-1.5 items-center px-2.5 py-1 bg-[#3F98FF4D] rounded-full">
+          <MaterialIcons name="verified" size={16} color="#3090FF" />
+          <Text className="text-xs font-proximanova-regular text-primary">
+            Verified
+          </Text>
+        </View>
+
+        <View className="flex-row gap-1.5 items-center px-2.5 py-1 bg-white rounded-full">
+          <FontAwesome name="star" size={16} color="#F1C400" />
+          <Text className="text-xs font-proximanova-regular">4</Text>
+        </View>
+
+        <View className="flex-row gap-1.5 items-center px-2.5 py-1 bg-white rounded-full">
+          <Text className="text-xs font-proximanova-regular">Full Time</Text>
+        </View>
+
+        <View className="flex-row gap-1.5 items-center px-2.5 py-1 bg-white rounded-full">
+          <Text className="text-xs font-proximanova-regular">2km away</Text>
+        </View>
+      </View>
+
+      <Image
+        source={require("@/assets/images/dotted-line.svg")}
+        style={{
+          height: 1,
+          width: "100%",
+          marginVertical: 10,
+        }}
+        contentFit="contain"
+      />
+
+      {/* bottom */}
+      <View className="flex-row justify-between">
+        {/* left */}
+        <View className="flex-row gap-2.5 items-center">
+          <View className="h-10 w-10 bg-white rounded-full flex-row items-center justify-center">
+            <Image
+              source={require("@/assets/images/messages-fill.svg")}
+              contentFit="contain"
+              style={{ height: 22, width: 22 }}
+            />
+          </View>
+
+          <Image
+            source={require("@/assets/images/vertical-line.svg")}
+            style={{
+              height: 18,
+              width: 0.5,
+            }}
+          />
+
+          <View className="h-10 w-10 bg-white rounded-full flex-row items-center justify-center">
+            <Image
+              source={require("@/assets/images/messages-fill.svg")}
+              contentFit="contain"
+              style={{ height: 22, width: 22 }}
+            />
+          </View>
+        </View>
+
+        {/* right */}
+        <SmallButton title="View Profile" />
+      </View>
+    </View>
+  );
+};
+
+export default BusinessJobCard;
