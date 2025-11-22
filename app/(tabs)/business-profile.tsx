3d7d4db214c87fc2c1f34ca940064dc694425aee
xyryc <amdanikzz@gmail.com>
2025-11-22 09:37:10 +0600

    settings menu navigation fix

--- app/(tabs)/business-profile.tsx
+++ app/(tabs)/business-profile.tsx
@@ -13,7 +13,7 @@ import {
 } from "@expo/vector-icons";
 import { Image } from "expo-image";
 import { router } from "expo-router";
-import React, { useState } from "react";
+import { useState } from "react";
 import { ScrollView, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
